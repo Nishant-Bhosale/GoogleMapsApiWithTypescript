a81055276857361f58c33f8cfa939740828905df
Nishant <nishantbhosale244@gmail.com>
2021-08-12 11:15:03 +0530

    Added google api and axios requests

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,37 @@
+import axios from "axios";
+
 const form = document.querySelector("form")! as HTMLFormElement;
 const addressInput = document.getElementById("address")! as HTMLInputElement;
 
-form.addEventListener("submit", formSubmitHandler);
+const GOOGLE_API_KEY: string = "AIzaSyCa92-mf0b-R0TqVnG4dMUBNHjQk3V0lEg";
+
+type GoogleGeocodingResponse = {
+	results: { geometry: { location: { lat: number; lng: number } } }[];
+	status: "OK" | "ZERO_RESULTS";
+};
 
 const formSubmitHandler = (event: Event): void => {
 	event.preventDefault();
+
+	const input = addressInput.value;
+
+	axios
+		.get<GoogleGeocodingResponse>(
+			`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+				input,
+			)}&key=${GOOGLE_API_KEY}`,
+		)
+		.then((response) => {
+			if (response.data.status !== "OK") {
+				throw new Error("Unable to get Address");
+			} else {
+				const coordinates = response.data.results[0].geometry.location;
+			}
+		})
+		.catch((err) => {
+			alert(err.message);
+			console.log(err);
+		});
 };
+
+form.addEventListener("submit", formSubmitHandler);
